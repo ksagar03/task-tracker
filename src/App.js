8cde89857750c1079e2956834a2541a87aeec071
ksagar03 <sagar22krishna03@gmail.com>
2022-06-20 20:10:31 +0530

    started new project on React

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+     
+        <h1 placeholder='centre'>
+          Hello this is sagar
+        </h1>
+
+    </div>
+  )
+}
+
+export default App;
