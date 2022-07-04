134127cd9ec75094599edd1bb685422726531b4a
ksagar03 <sagar22krishna03@gmail.com>
2022-07-04 21:00:12 +0530

    CRUD operations to add data and update reminder

--- src/App.js
+++ src/App.js
@@ -31,17 +31,44 @@ function App() {
       return data
     }
 
+    // this below will return single data based on requested id.
+    const fetchsingledata = async(id) =>{
+      const res = await fetch(`http://localhost:5000/tasks/${id}`)
+      const data= await res.json()
+       /*await-> it is an operator which waits for the promise this operator can only be used in 
+        async function.
+        async makes a function return a Promise
+        await makes a function wait for a Promise */
+        return data
+      }
+
 
   //  creating function to save task
   // SAVE TASK
 
-  const savetask = (task) => {
-    const id = Math.floor(Math.random() * 100) + 1; // this line will return
-    // numbers 1-100
-    const newTask = { id, ...task };
-    // here '...task' is a spread operator which includes all the value
-    // of that array task
-    setTasks([...tasks, newTask]);
+  const savetask = async (task) => {
+    const res= await fetch('http://localhost:5000/tasks',{
+      method:'POST',
+      headers:{
+        "Content-type":"application/json"
+      },
+      body: JSON.stringify(task)
+
+  })
+
+    const data= await res.json()
+    setTasks([...tasks,data])
+
+    
+    // Just for reference
+    // In bellow code we were creating random id but in above code json automaically creates id for
+    //  the data.
+    // const id = Math.floor(Math.random() * 100) + 1; // this line will return
+    // // numbers 1-100
+    // const newTask = { id, ...task };
+    // // here '...task' is a spread operator which includes all the value
+    // // of that array task
+    // setTasks([...tasks, newTask]);
   };
 
   // now let us write a code to delete task(here we are just filtering it out the non deleted task)
@@ -54,11 +81,23 @@ function App() {
   };
 
   // creating reminder notification
-  const onReminder = (id) => {
+  const onReminder = async (id) => {
     //  console.log('reminder is working',id)
+    const onremindertask= await fetchsingledata(id)
+    const updated_data={...onremindertask, Reminder:!onremindertask.Reminder}
+    const res= await fetch(`http://localhost:5000/tasks/${id}`,{
+      method: 'PUT',
+      headers:{
+        'Content-type': 'Application/json',
+      
+      },
+      body: JSON.stringify(updated_data)
+      
+    })
+    const data = await res.json()
     setTasks(
       tasks.map((task) =>
-        task.id === id ? { ...task, Reminder: !task.Reminder } : task
+        task.id === id ? { ...task, Reminder: data.Reminder } : task
       )
     );
   };
